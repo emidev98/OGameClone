4145482b3abd75424f2a0077fee64ca43ed87251
fmolins <frankii267mc@gmail.com>
2018-04-12 12:57:51 +0200

    Added ship types table seeder

---
+++ app/database/seeds/ShipTypesTableSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ShipTypesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      DB::table('ship_types')->insert([
+          'name' => 'Cazador ligero',
+          'attack_points' => 65,
+          'armor' => 10,
+          'capacity' => 50,
+          'speed' => 0.5,
+          'construct_time' => 0,
+          'hp' => 6000,
+      ]);
+      DB::table('ship_types')->insert([
+          'name' => 'Cazador pesado',
+          'attack_points' => 195,
+          'armor' => 25,
+          'capacity' => 100,
+          'speed' => 0.8,
+          'construct_time' => 0,
+          'hp' => 15000,
+      ]);
+      DB::table('ship_types')->insert([
+          'name' => 'Crucero',
+          'attack_points' => 410,
+          'armor' => 40,
+          'capacity' => 800,
+          'speed' => 0.4,
+          'construct_time' => 0,
+          'hp' => 39000,
+      ]);
+    }
+}
