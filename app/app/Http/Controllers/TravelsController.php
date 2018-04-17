9b400f21104a4f4c7c705ccc90f90a5f201bf445
Emi Harastasan <eharastasan@gmail.com>
2018-04-17 15:30:42 +0200

    Feature_update seeder and travels

--- app/app/Http/Controllers/TravelsController.php
+++ app/app/Http/Controllers/TravelsController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Planet;
 use App\Travel;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class TravelsController extends Controller
@@ -26,21 +25,13 @@ class TravelsController extends Controller
   }
 
   public function createTravel(Planet $planet){
-    /*DB::table('travels')->insert([
-           'origin_planet_id' => Auth::user()->planets[0]->id,
-           'destination_planet_id' => $planet->id,
-           'start_date' => NOW(),
-           'end_date' => NOW(),
-           'travel_type' => 'attack',
-           'fleet_id' => 1,
-     ]);*/
      $travel = new Travel;
      $travel->origin_planet_id = Auth::user()->planets[0]->id;
      $travel->destination_planet_id = $planet->id;
      $travel->start_date = NOW();
      $travel->end_date = NOW();
      $travel->travel_type = 'attack';
-     $travel->fleet_id = 1;
+     $travel->fleet_id = null;
      $travel->save();
   }
 }
