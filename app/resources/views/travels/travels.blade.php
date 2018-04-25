5a9375f4aa24587b5947d8a639a9891acf7a52f3
FrancescMolins <frankii267mc@gmail.com>
2018-04-25 18:01:59 +0200

    Deleted content of travel view

--- app/resources/views/travels/travels.blade.php
+++ app/resources/views/travels/travels.blade.php
@@ -1,11 +1,1 @@
-@extends('home')
 
-@section('inner-content')
-<ul>
-@foreach ($planets as $planet)
-    <!--<li><a href="{{route('travels-show', $planet)}}">{{$planet}}</a></li>-->
-    <li><a href="{{route('make-travel', $planet)}}">{{$planet}}</a></li>
-@endforeach
-</ul>
-
-@endsection
