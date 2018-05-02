a37d44802c630e9bcfcef4d4262133bf50493a65
Emi Harastasan <eharastasan@gmail.com>
2018-05-02 20:11:48 +0200

    Feature_disable planet input

---
+++ app/public/js/pages/planet.js
@@ -0,0 +1,11 @@
+
+function toggleEditMode(){
+    var input = document.getElementById("planetName");
+    input.setAttribute("disabled","");
+}
+
+function init(){
+    toggleEditMode();
+}
+
+// init();
--- app/resources/views/planet/planet.blade.php
+++ app/resources/views/planet/planet.blade.php
@@ -1,14 +1,15 @@
 @extends('app')
 @section('content')
 <div id="planet" class="container card">
-    <p>Planet: {{$planet->name}}</p>
-    <form class="form-horizontal" method="GET" action="{{route('change-planet-name', $planet)}}">
+    <form class="planet-header" method="GET" action="{{route('change-planet-name', $planet)}}">
         {{ csrf_field() }}
         <div class="form-group">
-            <input id="planetName" type="text" class="form-control" name="planetName" value="" required autofocus>
+            <h5>Planet:</h5>
+            <input id="planetName" type="text" class="form-control" name="planetName" value="{{$planet->name}}" required autofocus>
             <button type="submit" class="btn btn-primary light-blue darken-3">Change planet name</button>
         </div>
     </form>
+
     <p>Solar System: {{$planet->solar_system}}</p>
     <p>Position: {{$planet->position}}</p>
     <p>Planet Fleet:
