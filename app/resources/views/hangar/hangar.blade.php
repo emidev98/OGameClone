4cb5150491f168a4154ec24f3c6522be83813c30
Emi Harastasan <eharastasan@gmail.com>
2018-05-03 16:21:07 +0200

    Feature_hangar

--- app/resources/views/hangar/hangar.blade.php
+++ app/resources/views/hangar/hangar.blade.php
@@ -5,10 +5,8 @@
   @if (!$planet->has_hangar)
 
     <div id="create-hangar-section">
-      <p id="create-hangar-text"><b>If you create an hangar you will be able to build your fleet. Then you can make travels to attack, spy, transport, colonize or deploy people on other planets.</b></p>
-      <div id="create-hangar-button">
-        <a href="{{route('create-hangar', $planet)}}">Create Hangar</a>
-      </div>
+        <small>*If you create an hangar you will be able to build your fleet. Then you can make travels to attack, spy, transport, colonize or deploy people on other planets.</small>
+        <a class="btn btn-primary" href="{{route('create-hangar', $planet)}}">Create Hangar</a>
     </div>
 
   @else
